fa510e07b9f30508229c62e49fce8b5f57b683c6
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-02-14 21:22:30 +0700

    feat: add question for isSubArray problem

---
+++ 1-isSubArray/isSubArray.py
@@ -0,0 +1,23 @@
+"""
+Day 1 - 14 Februari 2025
+
+isSubArray: mengecek apakah array B merupakan bagian dari array A.
+
+> Testcase (1)
+ArrA = [X, Y, A, B, C]
+ArrB = [A, B, C]
+Output: True
+> Testcase (2)
+ArrA = [H, I, J, K, L]
+ArrB = [A, B, C]
+Output: False
+> Testcase (3)
+ArrA = [C, X, Y, A, B]
+ArrB = [A, B, C]
+Output: False
+> Testcase (4)
+ArrA = [X, Y, A, B, C]
+ArrB = [C, B, A]
+Output: False
+
+"""
