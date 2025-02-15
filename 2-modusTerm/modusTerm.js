964705968272ae13cda9837e159cd4125cfe4ac8
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-02-15 08:24:01 +0700

    feat: add question for modusTerm problem

---
+++ 2-modusTerm/modusTerm.js
@@ -0,0 +1,23 @@
+/*
+Day 2 - 15 Februari 2025
+
+ModusTerm(matrixA) -> modTerm & sumModTerm
+- menemukan kolom(term) dengan kemunculan terbanyak pada (Document Term) Matrix A
+- abaikan elemen yg bernilai -999
+modTerm: indeks kolom terbanyak [dimulai dari 1]
+sumModTerm: total kemunculan di kolom terbanyak
+
+> Testcase
+A = [
+    [0, 6, 2, 1, -999],
+    [8, 9, 6, 2, -999],
+    [1, 5, 4, 0, -999]
+]
+> Output:
+modTerm = 2  (indeks kolom(term) terbanyak [dimulai dari 1])
+sumModTerm = 20  (total kemunculan di kolom(term) terbanyak)
+> Penjelasan:
+Term/kolom dengan kemunculan terbanyak adalah term/kolom ke-2
+dengan total kemunculan 20 (6+9+5).
+
+*/
