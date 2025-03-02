400281ff454552aa3c19a2a21deb271c791a3fe1
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-03-02 15:04:15 +0700

    feat: add solution for remove-element problem

--- 11-removeElement/removeElement.js
+++ 11-removeElement/removeElement.js
@@ -3,3 +3,20 @@
 // https://leetcode.com/problems/remove-element/
 
 // Solution
+const removeElement = (nums, val) => {
+    let k = 0; // Pointer for placing non-val elements
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
+
+// Example usage:
+let nums = [3, 2, 2, 3];
+let val = 3;
+let k = removeElement(nums, val);
+console.log(k);       // Output: 2
+console.log(nums.slice(0, k)); // Output: [2, 2]
--- 11-removeElement/removeElement.py
+++ 11-removeElement/removeElement.py
@@ -3,3 +3,19 @@
 # https://leetcode.com/problems/remove-element/
 
 # Solution
+from typing import List
+
+def removeElement(nums: List[int], val: int) -> int:
+    k = 0  # Pointer for placing non-val elemetns
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+    return k
+
+# Example usage:
+nums = [3, 2, 2, 3]
+val = 3
+k = removeElement(nums, val)
+print(k)  # Output: 2
+print(nums[:k])  # Output: [2, 2]
