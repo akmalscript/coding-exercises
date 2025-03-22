eff5f155ea0506d31b2c20c012575a68b95ab8bb
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-03-22 12:24:10 +0700

    feat: add solutions for binary-tree-inorder-traversal problem

--- 21-binTreeInorderTraversal.py
+++ 21-binTreeInorderTraversal.py
@@ -3,3 +3,37 @@
 # https://leetcode.com/problems/binary-tree-inorder-traversal/
 
 # Solution
+from typing import Optional, List
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = leftb
+#         self.right = right
+class Solution:
+    # Solution 1 (Recursive)
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+    
+    # Solution 2 (Iterative using Stack)
+    def inorderTraversal2(self, root: Optional[TreeNode]) -> List[int]:
+        stack, result = [], []  # Stack for traversal, result list to store values
+        current = root  # Start from the root node
+
+        while current or stack:
+            # Traverse to the leftmost node
+            while current:
+                stack.append(current)  # Push current node to the stack
+                current = current.left  # Move to the left subtree
+            
+            # Process the last node from the stack
+            current = stack.pop()  # Take the last node
+            result.append(current.val)  # Store the node's value
+            
+            # Move to the right subtree
+            current = current.right
+        
+        return result  # Return the inorder traversal result
