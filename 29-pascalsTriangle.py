b90bfa3d9222bc3c0a409fa52414e25dea71a915
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-04-07 08:17:34 +0700

    feat: add solution for pascals-triangle problem

--- 29-pascalsTriangle.py
+++ 29-pascalsTriangle.py
@@ -3,3 +3,16 @@
 # https://leetcode.com/problems/pascals-triangle/
 
 # Solution
+from typing import List
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+        
+        for i in range(numRows):
+            row = [1] * (i + 1)  # Create a row filled with 1s
+            for j in range(1, i):  # Compute inner elements
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            triangle.append(row)
+        
+        return triangle
