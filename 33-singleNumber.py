d7e18f6ba5951c8a0d0df5b4c78c07b17587e561
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-04-15 14:16:49 +0700

    feat: add solution for single-number problem

--- 33-singleNumber.py
+++ 33-singleNumber.py
@@ -3,3 +3,11 @@
 # https://leetcode.com/problems/single-number/
 
 # Solution
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
