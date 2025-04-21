35ecf5de5b6d669950aade96de8af025610c0483
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-04-21 14:07:42 +0700

    feat: add solutions for binary-tree-postorder-traversal problem

--- 36-binTreePostorderTraversal.py
+++ 36-binTreePostorderTraversal.py
@@ -3,3 +3,39 @@
 # https://leetcode.com/problems/binary-tree-postorder-traversal/
 
 # Solution
+from typing import Optional, List
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Solution 1 (Recursive)
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def postorder(node):
+            if not node:
+                return []
+            return postorder(node.left) + postorder(node.right) + [node.val]
+        
+        return postorder(root)
+
+    # Solution 2 (Iterative - using two stacks)
+    def postorderTraversal2(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        stack = [root]
+        result = []
+
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        return result[::-1]  # reverse the result to get postorder
