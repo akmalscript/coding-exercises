76dba256bebd3b186754fa5e5fb50d75a710d391
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-04-27 11:11:19 +0700

    feat: add solution for majority-element problem

--- 39-majorityElement.py
+++ 39-majorityElement.py
@@ -3,3 +3,15 @@
 # https://leetcode.com/problems/majority-element/
 
 # Solution
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res, count = 0, 0
+
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
+
+        return res
