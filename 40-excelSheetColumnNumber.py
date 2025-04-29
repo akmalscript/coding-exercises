97625cc5fc4c9b3e5e8ebc6e74fdb7add76064ee
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-04-29 14:42:36 +0700

    feat: add solution for excel-sheet-column-number problem

--- 40-excelSheetColumnNumber.py
+++ 40-excelSheetColumnNumber.py
@@ -3,3 +3,9 @@
 # https://leetcode.com/problems/excel-sheet-column-number/
 
 # Solution
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
+        for char in columnTitle:
+            result = result * 26 + (ord(char) - ord('A') + 1)
+        return result
