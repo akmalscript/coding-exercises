92a6796f7aa5388dc8a128cb9114c625d7281891
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-05-09 07:55:52 +0700

    feat: add solution for reverse-bits problem

--- 45-reverseBits.py
+++ 45-reverseBits.py
@@ -3,3 +3,10 @@
 # https://leetcode.com/problems/reverse-bits/
 
 # Solution
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            bit = (n >> i) & 1  # Extract the i-th bit from n
+            res = res | (bit << (31 - i))  # Place the bit at the reversed position
+        return res
