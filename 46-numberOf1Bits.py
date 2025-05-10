01b118bce10c203d45ba286f2c109c410411a26d
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-05-11 06:56:58 +0700

    feat: add solutions for number-of-1-bits problem

--- 46-numberOf1Bits.py
+++ 46-numberOf1Bits.py
@@ -3,3 +3,19 @@
 # https://leetcode.com/problems/number-of-1-bits/
 
 # Solution
+class Solution:
+    # Solution 1 (Basic Bitwise Method)
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            count += n & 1  # add 1 if the last bit is 1
+            n >>= 1         # shift bits to the right
+        return count
+    
+    # Solution 2 (Brian Kernighan's algorithm)
+    def hammingWeight2(self, n: int) -> int:
+        count = 0
+        while n:
+            n &= (n - 1)  # remove the rightmost set bit
+            count += 1
+        return count
