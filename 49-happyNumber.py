338a39a465889bce47b912da01b9335046f2465a
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-05-17 19:55:03 +0700

    feat: add solution for happy-number problem

--- 49-happyNumber.py
+++ 49-happyNumber.py
@@ -3,3 +3,17 @@
 # https://leetcode.com/problems/happy-number/
 
 # Solution
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = self.get_next(n)
+        return n == 1
+    def get_next(self, number: int) -> int:
+        total = 0
+        while number > 0:
+            digit = number % 10
+            total += digit * digit
+            number //= 10
+        return total
