b078a512866c6ea5c7684806565d575a0d3c8277
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-05-25 22:04:24 +0700

    feat: add solution for contains-duplicate problem

--- 53-containsDuplicate.py
+++ 53-containsDuplicate.py
@@ -3,3 +3,14 @@
 # https://leetcode.com/problems/contains-duplicate/
 
 # Solution
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
