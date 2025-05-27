794c777edf91af7d60ca36298ac6a8d2e5fbbff6
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-05-27 22:34:48 +0700

    feat: add solution for contains-duplicate-ii problem

--- 54-containsDuplicate2.py
+++ 54-containsDuplicate2.py
@@ -3,3 +3,15 @@
 # https://leetcode.com/problems/contains-duplicate-ii/
 
 # Solution
+from typing import List
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        index_map = {}  # Stores the last seen index of each number
+        
+        for i, num in enumerate(nums):
+            if num in index_map and i - index_map[num] <= k:
+                return True
+            index_map[num] = i  # Update the last seen index
+        
+        return False
