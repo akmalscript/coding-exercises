04b7932edcdaecb0ea84c59b6bf051fde6ab3003
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-06-04 09:56:51 +0700

    feat: add solution for summary-ranges problem

--- 58-summaryRanges.py
+++ 58-summaryRanges.py
@@ -3,3 +3,30 @@
 # https://leetcode.com/problems/summary-ranges/
 
 # Solution
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
+        n = len(nums)
+        if n == 0:
+            return result
+        
+        start = nums[0]
+        
+        for i in range(1, n):
+            # If the current number is not consecutive
+            if nums[i] != nums[i - 1] + 1:
+                # Add the range to the result
+                if start == nums[i - 1]:
+                    result.append(f"{start}")
+                else:
+                    result.append(f"{start}->{nums[i - 1]}")
+                # Update the start to the current number
+                start = nums[i]
+        
+        # Add the last range
+        if start == nums[-1]:
+            result.append(f"{start}")
+        else:
+            result.append(f"{start}->{nums[-1]}")
+        
+        return result
