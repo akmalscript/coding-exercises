ef9efb955ab50e1d8df21541753fa625f9d567d0
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-06-10 08:56:25 +0700

    feat: add solution for palindrome-linked-list problem

--- 61-palindromeLinkedList.py
+++ 61-palindromeLinkedList.py
@@ -3,3 +3,41 @@
 # https://leetcode.com/problems/palindrome-linked-list/
 
 # Solution
+from typing import Optional
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return True
+        
+        slow = head
+        fast = head
+    
+        # Step 1: Find the middle (slow)
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Reverse the second half of the list
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+
+        # Step 3: Compare the first and the reversed second half (check palindrome)
+        first = head
+        second = prev
+        while second:
+            if first.val != second.val:
+                return False
+            first = first.next
+            second = second.next
+
+        return True
