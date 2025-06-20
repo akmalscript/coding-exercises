5e25feedf2ff49fbbfc8cd8f4e0e638f40f395f8
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-06-20 11:01:43 +0700

    feat: add solution for missing-number problem

--- 66-missingNumber.py
+++ 66-missingNumber.py
@@ -3,3 +3,10 @@
 # https://leetcode.com/problems/missing-number/
 
 # Solution
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        total = n * (n + 1) // 2
+        return total - sum(nums)
