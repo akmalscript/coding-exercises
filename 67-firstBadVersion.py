3c1c23e14bf3a4d1bce1e0596395c53d089e3c17
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-06-22 04:23:31 +0700

    feat: add solution for first-bad-version problem

--- 67-firstBadVersion.py
+++ 67-firstBadVersion.py
@@ -3,3 +3,16 @@
 # https://leetcode.com/problems/first-bad-version/
 
 # Solution
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+        while left < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid  # mid might be the first bad version
+            else:
+                left = mid + 1  # first bad version must be after mid
+        return left  # or right, since left == right
