fc9159f3491c47de17cd8689931f0f4908a6c21f
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-07-06 12:01:35 +0700

    feat: add solution for power-of-four problem

--- 74-powerOfFour.py
+++ 74-powerOfFour.py
@@ -3,3 +3,8 @@
 # https://leetcode.com/problems/power-of-four/
 
 # Solution
+from math import log
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return n > 0 and log(n, 4) % 1 == 0
