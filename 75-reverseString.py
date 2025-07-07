f4c62427e57abeb78cf473e0dfd431e8ab66db68
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-07-08 06:16:18 +0700

    feat: add solutions for reverse-string problem

--- 75-reverseString.py
+++ 75-reverseString.py
@@ -2,4 +2,43 @@
 # 344. Reverse String
 # https://leetcode.com/problems/reverse-string/
 
-# Solution
+# Solutions
+from typing import List
+
+class Solution:
+    # Solution 1 (Two Pointers)
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+       # Time: O(n)  Space: O(1)
+        l, r = 0, len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l, r = l + 1, r - 1
+    # Solution 2 (Stack)
+    def reverseString2(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # Time: O(n)  Space: O(n)
+        stack = []
+        for c in s:
+            stack.append(c)
+
+        i = 0
+        while stack:
+            s[i] = stack.pop()
+            i += 1
+    # Solution 3 (Recursion)
+    def reverseString3(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # Time: O(n)  Space: O(n)
+        def reverse(l, r):
+            if l < r:
+                s[l], s[r] = s[r], s[l]
+                reverse(l + 1, r - 1)
+
+        reverse(0, len(s) - 1)
