cd926801f99571e546ed38daa03ee08949560e2b
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-07-12 07:11:10 +0700

    feat: add solution for intersection-of-two-arrays problem

--- 77-intersectionOfTwoArrays.py
+++ 77-intersectionOfTwoArrays.py
@@ -3,3 +3,15 @@
 # https://leetcode.com/problems/intersection-of-two-arrays/
 
 # Solution
+from typing import List
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        seen = set(nums1)  # Convert nums1 to a set for O(1) lookups
+        res = []
+        # Iterate through nums2 and check for intersection
+        for n in nums2:
+            if n in seen:
+                res.append(n)      # Add to result if present in seen
+                seen.remove(n)     # Remove to ensure uniqueness
+        return res
