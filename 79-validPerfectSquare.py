7561a1339f23714dcb565c8f5956d170a805adc9
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-07-16 09:12:11 +0700

    feat: add solutions for valid-perfect-square problem

--- 79-validPerfectSquare.py
+++ 79-validPerfectSquare.py
@@ -2,4 +2,27 @@
 # 367. Valid Perfect Square
 # https://leetcode.com/problems/valid-perfect-square/
 
-# Solution
+# Solutions
+class Solution:
+    # Solution 1
+    def isPerfectSquare(self, num: int) -> bool:
+        # Binary search method: O(log n)
+        l, r = 1, num
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid > num:
+                r = mid - 1
+            elif mid * mid < num:
+                l = mid + 1
+            else:
+                return True
+        return False
+
+    # Solution 2
+    def isPerfectSquare2(self, num: int) -> bool:
+        # Brute-force method: O(sqrt(n))
+        for i in range(1, num + 1):
+            if i * i == num:
+                return True
+            if i * i > num:
+                return False
