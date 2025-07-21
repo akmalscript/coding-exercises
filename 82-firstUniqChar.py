5f0e3285b99be03ce6229fe32584a04c890d051e
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-07-22 06:17:58 +0700

    feat: add solution for first-unique-character-in-a-string problem

--- 82-firstUniqChar.py
+++ 82-firstUniqChar.py
@@ -3,3 +3,21 @@
 # https://leetcode.com/problems/first-unique-character-in-a-string/
 
 # Solution
+from collections import defaultdict
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        # Create a dictionary to count the frequency of each character
+        count = defaultdict(int)  # Default value for int is 0
+
+        # First pass: count each character's frequency
+        for c in s:
+            count[c] += 1
+
+        # Second pass: find the first character with a count of 1
+        for i, c in enumerate(s):
+            if count[c] == 1:
+                return i  # Return the index of the first unique character
+
+        # If no unique character is found, return -1
+        return -1
