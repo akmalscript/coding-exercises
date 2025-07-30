912cbb7836e6a0a4c3efb6138f0716c172579d0a
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2025-07-30 15:12:03 +0700

    feat: add solution for sum-of-left-leaves problem

--- 86-sumOfLeftLeaves.py
+++ 86-sumOfLeftLeaves.py
@@ -3,3 +3,20 @@
 # https://leetcode.com/problems/sum-of-left-leaves/
 
 # Solution
+from typing import Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        if root.left and root.left.left is None and root.left.right is None:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
