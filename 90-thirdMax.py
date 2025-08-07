03c53e608dcdf4269c4423b2507bf7f4ee10be31
Akmal FA <134951391+akmalscript@users.noreply.github.com>
2025-08-07 07:35:37 +0700

    feat: add solution for third-maximum-number problem

--- 90-thirdMax.py
+++ 90-thirdMax.py
@@ -3,3 +3,23 @@
 # https://leetcode.com/problems/third-maximum-number/
 
 # Solution
+from typing import List
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        first = second = third = float('-inf')
+        
+        for num in nums:
+            if num in (first, second, third):
+                continue
+            if num > first:
+                third = second
+                second = first
+                first = num
+            elif num > second:
+                third = second
+                second = num
+            elif num > third:
+                third = num
+        
+        return third if third != float('-inf') else first
