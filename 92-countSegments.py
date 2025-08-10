59a914173b444f10fa8619d5f49a069fad736a48
Akmal FA <134951391+akmalscript@users.noreply.github.com>
2025-08-11 06:19:00 +0700

    feat: add solutions for number-of-segments-in-a-string problem

--- 92-countSegments.py
+++ 92-countSegments.py
@@ -3,3 +3,22 @@
 # https://leetcode.com/problems/number-of-segments-in-a-string/
 
 # Solution
+class Solution:
+    # Solution 1 (Index-Based Segment Counting)
+    def countSegments(self, s: str) -> int:
+        count = 0
+        for i in range(len(s)):
+            if (i == 0 or s[i - 1] == " ") and s[i] != " ":
+                count += 1
+        return count
+    
+    # Solution 2 (Split-and-Filter Segment Counting)
+    def countSegments2(self, s: str) -> int:
+        parts = s.split(" ")
+        count = 0
+        
+        for part in parts:
+            if part != "":
+                count += 1
+        
+        return count
