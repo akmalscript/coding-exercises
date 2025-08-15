985a8ccd50490e7a4b7999d0724702a1cfc08b02
Akmal FA <134951391+akmalscript@users.noreply.github.com>
2025-08-15 08:43:01 +0700

    feat: add solution for find-all-numbers-disappeared-in-an-array problem

--- 94-findDisappearedNumbers.py
+++ 94-findDisappearedNumbers.py
@@ -3,3 +3,20 @@
 # https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
 # Solution
+from typing import List
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        # Step 1: Mark each number's corresponding index as visited
+        for n in nums:
+            i = abs(n) - 1
+            nums[i] = -1 * abs(nums[i])
+
+        # Step 2: Collect indices whose values are still positive
+        res = []
+        for i, n in enumerate(nums):
+            if n > 0:
+                res.append(i + 1)
+
+        # Step 3: Return all missing numbers
+        return res
