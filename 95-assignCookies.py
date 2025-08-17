a72a22084dd9009acc95a93f098266f72e33075c
Akmal FA <134951391+akmalscript@users.noreply.github.com>
2025-08-17 07:23:01 +0700

    feat: add solution for assign-cookies problem

--- 95-assignCookies.py
+++ 95-assignCookies.py
@@ -3,3 +3,19 @@
 # https://leetcode.com/problems/assign-cookies/
 
 # Solution
+from typing import List
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        i = j = 0
+        while i < len(g):
+            while j < len(s) and g[i] > s[j]:
+                j += 1
+            if j == len(s):
+                break
+            i += 1
+            j += 1
+        return i
