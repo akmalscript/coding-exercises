81a74fb379b0901a4e5e000e6f4399c4552bf30f
Akmal FA <134951391+akmalscript@users.noreply.github.com>
2025-08-19 06:30:40 +0700

    feat: add solutions for repeated-substring-pattern problem

--- 96-repeatedSubstringPattern.py
+++ 96-repeatedSubstringPattern.py
@@ -3,3 +3,24 @@
 # https://leetcode.com/problems/repeated-substring-pattern/
 
 # Solution
+class Solution:
+    # Solution 1 (Trick Method)
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in (s + s)[1:-1]
+
+    # Solution 2 (KMP prefix function Algorithm)
+    def repeatedSubstringPattern2(self, s: str) -> bool:
+        n = len(s)
+        lps = [0] * n  # Longest Prefix Suffix array
+        j = 0  # length of the previous longest prefix suffix
+
+        # Build the LPS array
+        for i in range(1, n):
+            while j > 0 and s[i] != s[j]:
+                j = lps[j - 1]
+            if s[i] == s[j]:
+                j += 1
+                lps[i] = j
+
+        l = lps[-1]  # length of the longest proper prefix which is also a suffix
+        return l != 0 and n % (n - l) == 0
