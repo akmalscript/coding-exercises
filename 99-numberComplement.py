fb91780c226b6b91d0f6a244c11a810dfb53b0b0
Akmal FA <134951391+akmalscript@users.noreply.github.com>
2025-08-25 06:55:07 +0700

    feat: add solution for number-complement problem

--- 99-numberComplement.py
+++ 99-numberComplement.py
@@ -3,3 +3,16 @@
 # https://leetcode.com/problems/number-complement/
 
 # Solution
+class Solution:
+    def findComplement(self, num: int) -> int:
+        # Find the bit length of the number
+        length = len(bin(num)) - 2   # bin(num) returns a string like '0b101'
+        
+        # Build a mask with all bits = 1
+        mask = 0
+        for i in range(length):
+            mask = mask * 2 + 1   # add 1 to the end of the mask bits
+        
+        # Flip bits using XOR
+        result = num ^ mask
+        return result
