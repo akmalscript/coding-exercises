1ac0d37a7c059b24d6bbbc8b6ffe2ff9dbc3aa1b
Akmal FA <134951391+akmalscript@users.noreply.github.com>
2025-08-27 06:11:11 +0700

    feat: add solution for license-key-formatting problem

--- 100-licenseKeyFormatting.py
+++ 100-licenseKeyFormatting.py
@@ -3,3 +3,17 @@
 # https://leetcode.com/problems/license-key-formatting/
 
 # Solution
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        # Remove all dashes and convert to uppercase
+        s = s.replace("-", "").upper()
+        
+        # Determine length of the first group
+        first_group_len = len(s) % k or k
+        
+        # Build result
+        result = s[:first_group_len]
+        for i in range(first_group_len, len(s), k):
+            result += "-" + s[i:i+k]
+        
+        return result
